ef44cc3118fe9018e4172c7c7670d3159e4dbf43
garloni <garloni@protonmail.com>
2020-09-15 00:27:04 +0200

    Collatz updated

--- sweigart/code/collatz.py
+++ sweigart/code/collatz.py
@@ -1,30 +1,24 @@
-"""
-Collatz sequence
-
-if number == even
-    print(number // 2)
-    return number // 2
-
-if number == odd
-    print(3 * number + 1)
-    return print
-
-"""
+# Collatz sequence
 
 
 def collatz(number):
     if (number % 2) == 0:
-        sequenza = number // 2
+        return number // 2
     else:
-        sequenza = 3 * number + 1
+        return 3 * number + 1
 
-    print(sequenza)
-    return sequenza
-
-
-print("Enter number: ")
-utente = int(input())
-collatz(utente)
 
 while True:
-    collatz(sequenza)
+    try:
+        print("Enter number: ")
+        utente = int(input())
+        break
+    except ValueError:
+        print("You must insert an integer")
+
+risposta = 2
+
+while risposta > 1:
+    risposta = collatz(utente)
+    print(risposta)
+    utente = risposta
